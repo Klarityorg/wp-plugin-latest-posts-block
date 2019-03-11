09317a7042fd858f566cfd62ba511fe9236279cd
Bruno Perel <bruno@klarity.org>
2019-03-11 10:29:26 +0100

    Bump version

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Description: Klarity latest posts block
  * Author: Klarity
  * Author URI: https://github.com/Klarityorg
- * Version: 1.1.0
+ * Version: 1.1.1
  * License: MIT
  *
  * @package Klarity
